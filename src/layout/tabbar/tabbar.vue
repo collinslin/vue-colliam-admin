681a03bbd4f9f5dc2ab791609ca6313fb4c998ef
林国梁 <1691603298@qq.com>
2021-04-14 00:15:19 +0800

    新增：新增tabbar菜单啊🎉

--- src/layout/tabbar/tabbar.vue
+++ src/layout/tabbar/tabbar.vue
@@ -1,139 +1,180 @@
 <template>
-	<el-tabs
-		class="my-tabs"
-		v-model="tabbarDataValue"
-		type="card"
-		@tab-click="chooseTabbar"
-		@tab-remove="removeTabbar"
-		@contextmenu.prevent="showTabbarMenu"
-	>
-		<el-tab-pane
-			v-for="item in tabbarData"
-			:key="item.path"
-			:label="item.name"
-			:name="item.path"
-			:closable="item.meta.isCloseTabbar == false ? false : true"
-		>
-		</el-tab-pane>
-	</el-tabs>
-	<div class="tabbar-menu-nav" v-show="showTabbarMenu">
-		<TabbarMenu></TabbarMenu>
-	</div>
+  <el-tabs
+    class="my-tabs"
+    v-model="tabbarDataValue"
+    type="card"
+    @tab-click="chooseTabbar"
+    @tab-remove="removeTabbar"
+    @contextmenu.prevent="showTabbarMenu"
+  >
+    <el-tab-pane
+      v-for="item in tabbarData"
+      :key="item.path"
+      :label="item.name"
+      :name="item.path"
+      :closable="item.meta.isCloseTabbar == false ? false : true"
+    >
+    </el-tab-pane>
+  </el-tabs>
+  <transition name="el-zoom-in-top">
+    <TabbarMenu
+      :left="left"
+      v-show="isShow"
+      @choose-tabbar-menu="chooseTabbarMenu"
+    ></TabbarMenu>
+  </transition>
 </template>
 
 <script lang="ts">
-	import {
-		computed,
-		ComputedRef,
-		defineComponent,
-		inject,
-	} from '@vue/runtime-core'
-	import { RouteRecordRaw, useRoute, useRouter } from 'vue-router'
-	import { useStore } from 'vuex'
-	import ArrRemove from 'lodash/remove'
-	import { Index } from '/@/type/store'
-	import TabbarMenu from './tabbarMenu.vue'
+  import {
+    computed,
+    ComputedRef,
+    defineComponent,
+    inject,
+    onMounted,
+    onUnmounted,
+    ref,
+  } from '@vue/runtime-core'
+  import { RouteRecordRaw, useRoute, useRouter } from 'vue-router'
+  import { useStore } from 'vuex'
+  import { Index } from '/@/type/store'
+  import TabbarMenu from './tabbarMenu.vue'
+  import { ElMessage } from 'element-plus'
 
-	export default defineComponent({
-		name: 'Tabbar',
-		components: { TabbarMenu },
-		setup() {
-			const store = useStore<Index>()
-			const route = useRoute()
-			const router = useRouter()
+  export default defineComponent({
+    name: 'Tabbar',
+    components: { TabbarMenu },
+    setup() {
+      const store = useStore<Index>()
+      const route = useRoute()
+      const router = useRouter()
 
-			const menuData = inject('menuData') as ComputedRef<RouteRecordRaw[]>
+      const menuData = inject('menuData') as ComputedRef<RouteRecordRaw[]>
 
-			const tabbarDataValue = computed({
-				get: () => route.path,
-				set: () => route.path,
-			})
+      const tabbarDataValue = computed({
+        get: () => route.path,
+        set: () => route.path,
+      })
 
-			const tabbarData = computed(() => store.state.setting?.tabbarData)
+      const tabbarData = computed(() => store.state.setting?.tabbarData)
 
-			const chooseTabbar = (tabbar: any) => {
-				router.push(tabbar.props.name)
-			}
+      const chooseTabbar = (tabbar: any) => {
+        router.push(tabbar.props.name)
+      }
 
-			const removeTabbar = (path: string) => {
-				menuData.value.forEach((menu: RouteRecordRaw | any) => {
-					if (path.includes(menu.path)) {
-						menu.active = false
-					} else {
-						menu.active = true
-					}
-				})
-				if (store.state.setting) {
-					store.state.setting.tabbarData = ArrRemove(
-						store.state.setting.tabbarData,
-						(item) => item.path !== path
-					)
+      const removeTabbar = (path: string) => {
+        if (path.includes('home')) {
+          ElMessage({
+            message: '首页无法删除',
+            type: 'error',
+          })
+          return
+        }
+        menuData.value.forEach((menu: RouteRecordRaw | any) => {
+          if (path.includes(menu.path)) {
+            menu.active = false
+          } else {
+            menu.active = true
+          }
+        })
+        if (store.state.setting) {
+          const index = store.state.setting.tabbarData.findIndex(
+            (item) => item.path == path
+          )
+          index != -1 && store.state.setting.tabbarData.splice(index, 1)
 
-					const pushPath =
-						store.state.setting.tabbarData[
-							store.state.setting.tabbarData.length - 1
-						].path
-					router.push(pushPath)
-				}
-			}
+          const pushPath =
+            store.state.setting.tabbarData[
+              store.state.setting.tabbarData.length - 1
+            ].path
+          router.push(pushPath)
+        }
+      }
 
-			const showTabbarMenu = (e: MouseEvent) => {
-				console.log(e.target)
-			}
+      const isShow = ref(false)
+      const left = ref(0)
+      const showTabbarMenu = (e: MouseEvent) => {
+        const target = e.target as HTMLElement
+        const ElOffsetLeft = target.offsetLeft
+        const ElOffsetWidth = target.offsetWidth / 2
+        left.value = ElOffsetLeft + ElOffsetWidth - 60
+        isShow.value = true
+      }
 
-			return {
-				route,
-				tabbarDataValue,
-				tabbarData,
-				chooseTabbar,
-				removeTabbar,
-				showTabbarMenu,
-			}
-		},
-	})
+      const refresh = () => {}
+      const myDelete = () => {
+        return removeTabbar(route.path)
+      }
+      const typeList: { [key: string]: any } = {
+        refresh,
+        myDelete,
+      }
+      const chooseTabbarMenu = (type: string) => {
+        typeList[type] && typeList[type]()
+      }
+
+      onMounted(() => {
+        window.addEventListener('click', () => {
+          isShow.value = false
+        })
+      })
+
+      onUnmounted(() => {
+        window.removeEventListener('click', () => {})
+      })
+
+      return {
+        route,
+        tabbarDataValue,
+        tabbarData,
+        chooseTabbar,
+        removeTabbar,
+        isShow,
+        left,
+        chooseTabbarMenu,
+        showTabbarMenu,
+      }
+    },
+  })
 </script>
 
+<style lang="scss" scoped></style>
 <style lang="scss" scoped>
-	.tabbar-menu-nav {
-		position: absolute;
-	}
-</style>
-<style lang="scss" scoped>
-	//element-ui
-	:deep(.el-tabs__header) {
-		margin: 0;
-		border: none !important;
-	}
-	:deep(.el-tabs__nav) {
-		border: none !important;
-	}
-	:deep(.el-tabs__item) {
-		height: 30px;
-		border-right-color: beige;
-		background-color: #ccdff2;
-		line-height: 30px;
-		margin: 0 2px;
-		border: none !important;
-		transition: all 0.3s !important;
-		&::before {
-			content: '';
-			display: inline-block;
-			width: 10px;
-			height: 10px;
-			background-color: rgb(10, 204, 114);
-			border-radius: 50%;
-			opacity: 0;
-			transition: all 0.2s;
-		}
-		&:hover {
-			background-color: #fff !important;
-		}
-	}
-	:deep(.is-active) {
-		background-color: #fff !important;
-		&::before {
-			opacity: 1;
-			transform: translateX(-7px);
-		}
-	}
+  //element-ui
+  :deep(.el-tabs__header) {
+    margin: 0;
+    border: none !important;
+  }
+  :deep(.el-tabs__nav) {
+    border: none !important;
+  }
+  :deep(.el-tabs__item) {
+    height: 30px;
+    border-right-color: beige;
+    background-color: #ccdff2;
+    line-height: 30px;
+    margin: 0 2px;
+    border: none !important;
+    transition: all 0.3s !important;
+    &::before {
+      content: '';
+      display: inline-block;
+      width: 10px;
+      height: 10px;
+      background-color: rgb(10, 204, 114);
+      border-radius: 50%;
+      opacity: 0;
+      transition: all 0.2s;
+    }
+    &:hover {
+      background-color: #fff !important;
+    }
+  }
+  :deep(.is-active) {
+    background-color: #fff !important;
+    &::before {
+      opacity: 1;
+      transform: translateX(-7px);
+    }
+  }
 </style>
--- src/layout/tabbar/tabbarMenu.vue
+++ src/layout/tabbar/tabbarMenu.vue
@@ -1,48 +1,90 @@
 <template>
-	<el-dropdown trigger="contextmenu">
-		<span class="el-dropdown-link"></span>
-		<template #dropdown>
-			<el-dropdown-menu>
-				<el-dropdown-item
-					v-for="item in dropdownMenu"
-					:key="item.name"
-					:icon="item.icon"
-					>{{ item.name }}</el-dropdown-item
-				>
-			</el-dropdown-menu>
-		</template>
-	</el-dropdown>
+  <div class="dropdown-menu" :style="{ left: 20 + left + 'px', top: '105px' }">
+    <div
+      class="dropdown-item"
+      v-for="item in dropdownMenu"
+      :key="item.name"
+      @click="chooseTabbarMenu(item.type)"
+    >
+      <el-icon :class="item.icon"></el-icon>
+      {{ item.name }}
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-	import { defineComponent, reactive } from '@vue/runtime-core'
+  import { defineComponent, reactive } from '@vue/runtime-core'
 
-	export default defineComponent({
-		name: 'TabbarMenu',
-		setup() {
-			const dropdownMenu = reactive([
-				{
-					icon: 'el-icon-refresh',
-					name: '刷新页面',
-				},
-				{
-					icon: 'el-icon-delete',
-					name: '删除本页',
-				},
-			])
+  export default defineComponent({
+    name: 'TabbarMenu',
+    props: {
+      left: {
+        type: Number,
+        required: true,
+      },
+    },
+    setup(props, { emit }) {
+      const dropdownMenu = reactive([
+        {
+          icon: 'el-icon-refresh',
+          name: '刷新页面',
+          type: 'refresh',
+        },
+        {
+          icon: 'el-icon-delete',
+          name: '删除本页',
+          type: 'myDelete',
+        },
+      ])
 
-			return {
-				dropdownMenu,
-			}
-		},
-	})
+      const chooseTabbarMenu = (type: string) => {
+        emit('choose-tabbar-menu', type)
+        // typeList[type] && typeList[type]()
+      }
+
+      return {
+        dropdownMenu,
+        chooseTabbarMenu,
+      }
+    },
+  })
 </script>
 
-<style lang="scss" scoped></style>
 <style lang="scss" scoped>
-	:deep(.el-dropdown-link) {
-		display: block !important;
-		width: 100px;
-		height: 30px;
-	}
+  .dropdown-menu {
+    position: absolute;
+    width: 120px;
+    top: 35px;
+    border: 1px solid rgb(233, 233, 235);
+    background-color: #fff;
+    z-index: 2;
+    text-align: center;
+    box-shadow: 0px -4px 20px 0px #0000001c;
+    transition: all 0.2s;
+    &::before {
+      content: '';
+      position: absolute;
+      width: 0;
+      height: 0;
+      left: 0;
+      right: 0;
+      margin: auto;
+      border: 10px solid rgb(233, 233, 235);
+      border-color: transparent transparent #fff transparent;
+      transform: translateY(-100%);
+      z-index: 3;
+    }
+    .dropdown-item {
+      padding: 10px 5px;
+      margin: 5px 0;
+      font-size: 18px;
+      background-color: #fff;
+      transition: all 0.3s;
+      cursor: pointer;
+      &:hover {
+        background-color: rgba(217, 236, 255, 0.6);
+        color: #409eff;
+      }
+    }
+  }
 </style>
